7c3b6edc81a362c68b1786b5bd6db2096adc6291
maksim.lastenko <maksim.lastenko@ax-team.com>
2023-02-06 01:19:18 +0600

    Добавить ShellMethodAvailability

--- hw4/src/main/java/ru/lastenko/studenttest/service/ShellCommands.java
+++ hw4/src/main/java/ru/lastenko/studenttest/service/ShellCommands.java
@@ -1,10 +1,15 @@
 package ru.lastenko.studenttest.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
 import org.springframework.shell.standard.ShellOption;
 import ru.lastenko.studenttest.i18n.LocaleToggle;
+
+import static org.springframework.shell.Availability.available;
+import static org.springframework.shell.Availability.unavailable;
 
 @ShellComponent
 @RequiredArgsConstructor
@@ -12,19 +17,31 @@ public class ShellCommands {
 
     private final ExamExecutor examExecutor;
     private final LocaleToggle localeToggle;
+    private boolean isReady = false;
 
-    @ShellMethod(key = {"start", "s", "e"}, value = "Start an exam.")
+    @ShellMethod(value = "Confirm of readiness for the test.", key = {"ready", "r"})
+    public void confirmReadiness() {
+        isReady = true;
+    }
+
+    @ShellMethod(value = "Start an exam.", key = {"start", "s", "e"})
+    @ShellMethodAvailability(value = "isReadinessConfirmed")
     public void startExam() {
         examExecutor.executeExam();
     }
 
-    @ShellMethod(key = {"locale", "l"}, value = "Show current locale.")
+    @ShellMethod(value = "Show current locale.", key = {"locale", "l"})
     public void showCurrentLocale() {
         localeToggle.showCurrentLocale();
     }
 
-    @ShellMethod(key = {"set locale", "sl"}, value = "Select locale.")
+    @ShellMethod(value = "Select locale.", key = {"set locale", "sl"})
     public void setLocale(@ShellOption() String locale) {
         localeToggle.setLocale(locale);
+    }
+
+    private Availability isReadinessConfirmed() {
+        String message = "it is required to confirm readiness for the test.";
+        return isReady ? available() : unavailable(message);
     }
 }
