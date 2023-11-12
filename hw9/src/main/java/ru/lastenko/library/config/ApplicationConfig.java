4fa75dd6057b8c81226e6cb7c05cc31b78036d5a
maksim.lastenko <maksim.lastenko@ax-team.com>
2023-11-12 16:29:01 +0600

    Копия 7-й домашней работы

---
+++ hw9/src/main/java/ru/lastenko/library/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package ru.lastenko.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.lastenko.library.service.IOService;
+import ru.lastenko.library.service.IOServiceStreams;
+import ru.lastenko.library.service.tostringconvertion.ToStringConversionHandler;
+
+import java.util.Scanner;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    @SuppressWarnings("java:S106")
+    public IOService ioService(ToStringConversionHandler toStringConversionHandler) {
+        var scanner = new Scanner(System.in);
+        return new IOServiceStreams(System.out, scanner, toStringConversionHandler);
+    }
+}
