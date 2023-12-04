05390409e7bf218c456b3790a156821dcccc3d31
maksim.lastenko <maksim.lastenko@ax-team.com>
2023-12-04 18:07:33 +0600

    Инициализация проекта hw11 (library-web-flux)

---
+++ hw11/src/main/java/ru/lastenko/librarywebflux/LibraryWebFluxApplication.java
@@ -0,0 +1,13 @@
+package ru.lastenko.librarywebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebFluxApplication.class, args);
+	}
+
+}
---
+++ hw11/src/test/java/ru/lastenko/librarywebflux/LibraryWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lastenko.librarywebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
