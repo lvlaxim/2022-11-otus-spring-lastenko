caec97a05e316967aa1e0f002af1350150258204
maksim.lastenko <maksim.lastenko@ax-team.com>
2023-12-20 13:16:44 +0600

    Рефакторинг - вернуться на @WebMvcTest

--- hw12/src/test/java/ru/lastenko/library/security/BookControllerSecurityTest.java
+++ hw12/src/test/java/ru/lastenko/library/security/BookControllerSecurityTest.java
@@ -1,16 +1,23 @@
 package ru.lastenko.library.security;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.lastenko.library.controller.BookController;
 import ru.lastenko.library.dto.BookDto;
+import ru.lastenko.library.service.AuthorService;
 import ru.lastenko.library.service.BookService;
+import ru.lastenko.library.service.GenreService;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,12 +25,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("Механизм безопасности должен")
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(BookController.class)
+@Import(SecurityConfiguration.class)
 class BookControllerSecurityTest {
 
     @MockBean
     private BookService bookService;
+
+    @MockBean
+    private AuthorService authorService;
+
+    @MockBean
+    private GenreService genreService;
+
+    @MockBean
+    private UserService userService;
 
     @Autowired
     private MockMvc mvc;
