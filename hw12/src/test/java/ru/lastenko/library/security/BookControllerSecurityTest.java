c1868ee58fe846ee94ea8b7b8f81d53a38164cbf
maksim.lastenko <maksim.lastenko@ax-team.com>
2023-12-20 13:23:11 +0600

    Рефакторинг - убрать лишние импорты

--- hw12/src/test/java/ru/lastenko/library/security/BookControllerSecurityTest.java
+++ hw12/src/test/java/ru/lastenko/library/security/BookControllerSecurityTest.java
@@ -1,17 +1,13 @@
 package ru.lastenko.library.security;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.lastenko.library.controller.BookController;
 import ru.lastenko.library.dto.BookDto;
