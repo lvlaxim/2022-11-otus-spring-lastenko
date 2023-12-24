8b615101f29dcc2b202d85541f56594364832148
maksim.lastenko <maksim.lastenko@ax-team.com>
2023-12-24 20:11:23 +0600

    Рефакторинг тестов безопасности с учетом ролей и авторизации

--- hw13/src/test/java/ru/lastenko/library/security/BookControllerSecurityTest.java
+++ hw13/src/test/java/ru/lastenko/library/security/BookControllerSecurityTest.java
@@ -1,13 +1,17 @@
 package ru.lastenko.library.security;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.lastenko.library.controller.BookController;
 import ru.lastenko.library.dto.BookDto;
@@ -15,6 +19,9 @@ import ru.lastenko.library.service.AuthorService;
 import ru.lastenko.library.service.BookService;
 import ru.lastenko.library.service.GenreService;
 
+import java.util.List;
+
+import static org.junit.platform.commons.util.StringUtils.isBlank;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -25,6 +32,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Import(SecurityConfiguration.class)
 class BookControllerSecurityTest {
 
+    static final String ROLE_SOMEBODY = "ROLE_SOMEBODY";
+    static final String ROLE_ADMIN = "ROLE_ADMIN";
+    static final String ROLE_USER = "ROLE_USER";
     @MockBean
     private BookService bookService;
 
@@ -40,31 +50,40 @@ class BookControllerSecurityTest {
     @Autowired
     private MockMvc mvc;
 
+    @AfterEach
+    void doAfterEach() {
+        SecurityContextHolder.clearContext();
+    }
+
     @ParameterizedTest
-    @ValueSource(strings = {
-            "/book",
-            "/book/new",
-            "/book/{id}"
+    @CsvSource({
+            "/book,",
+            "/book," + ROLE_USER,
+            "/book," + ROLE_ADMIN,
+            "/book," + ROLE_SOMEBODY,
+            "/book/new," + ROLE_USER,
+            "/book/new," + ROLE_ADMIN,
+            "/book/{id}," + ROLE_USER,
+            "/book/{id}," + ROLE_ADMIN
     })
-    @WithMockUser(username = "user")
-    @DisplayName("предоставить доступ аутентифицированному пользователю к GET-методу:")
-    void shouldGrantAccessForAuthorizedToGetMethod(String urlTemplate) throws Exception {
+    @DisplayName("предоставить доступ пользователю к GET-методу согласно роли:")
+    void shouldGrantAccessToGetMethodByRole(String urlTemplate, String role) throws Exception {
         long id = 1;
         var book = new BookDto();
         book.setId(id);
         when(bookService.getBy(book.getId())).thenReturn(book);
 
+        setUserWithRole(role);
         mvc.perform(get(urlTemplate, id))
                 .andExpect(status().isOk());
     }
 
     @ParameterizedTest
     @ValueSource(strings = {
-            "/book",
             "/book/new",
             "/book/1"
     })
-    @DisplayName("перенаправить на страницу входа пользователя без аутентификации обращающегося к GET-методу:")
+    @DisplayName("перенаправить на страницу входа пользователя без аутентификации, обращающегося к GET-методу:")
     void shouldRedirectToLoginPageForUnauthorizedToGetMethod(String urlTemplate) throws Exception {
         mvc.perform(get(urlTemplate))
                 .andExpect(status().is3xxRedirection())
@@ -73,12 +92,24 @@ class BookControllerSecurityTest {
 
     @ParameterizedTest
     @ValueSource(strings = {
+            "/book/new",
+            "/book/1"
+    })
+    @DisplayName("отказать в доступе пользователю с неподходящей ролью, обращающегося к GET-методу:")
+    void shouldDenyAccessToGetMethodForWrongRole(String urlTemplate) throws Exception {
+        setUserWithRole(ROLE_SOMEBODY);
+        mvc.perform(get(urlTemplate))
+                .andExpect(status().isForbidden());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
             "/book",
             "/book/delete"
     })
-    @WithMockUser(username = "user")
-    @DisplayName("предоставить доступ аутентифицированному пользователю к POST-методу:")
-    void shouldGrantAccessForAuthorizedToPostMethod(String urlTemplate) throws Exception {
+    @DisplayName("предоставить доступ пользователю с ролью 'ADMIN' к POST-методу:")
+    void shouldGrantAccessForAdminToPostMethod(String urlTemplate) throws Exception {
+        setUserWithRole(ROLE_ADMIN);
         mvc.perform(post(urlTemplate).param("id", "1"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlTemplate("/book"));
@@ -94,6 +125,27 @@ class BookControllerSecurityTest {
         mvc.perform(post(urlTemplate).param("id", "1"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "/book",
+            "/book/delete"
+    })
+    @DisplayName("отказать в доступе пользователю с неподходящей ролью, обращающегося к POST-методу:")
+    void shouldDenyAccessToPostMethodForWrongRole(String urlTemplate) throws Exception {
+        setUserWithRole(ROLE_SOMEBODY);
+        mvc.perform(post(urlTemplate).param("id", "1"))
+                .andExpect(status().isForbidden());
+    }
+
+    private void setUserWithRole(String role) {
+        if (isBlank(role)) {
+            return;
+        }
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                "username", null, List.of(new SimpleGrantedAuthority(role)));
+        SecurityContextHolder.getContext().setAuthentication(auth);
     }
 
 }
