61d8d6061a076177adff68d1e35e785b6dcf7dee
maksim.lastenko <maksim.lastenko@ax-team.com>
2024-01-20 18:03:28 +0600

    Инициализация проекта

---
+++ hw15/src/main/java/ru/lastenko/butterflyspringintegration/ButterflySpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ru.lastenko.butterflyspringintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButterflySpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButterflySpringIntegrationApplication.class, args);
+	}
+
+}
